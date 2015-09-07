0b5e7aecd3eb5cec4b5038088b2b34e9a58d7239
Daniel Ortiz <daniel.ortiz@tum.de>
2015-09-07 15:57:08 +0200

    implemented independence of number of cores

--- src/AutopinPlus/Monitor/PageMigrate/Main.cpp
+++ src/AutopinPlus/Monitor/PageMigrate/Main.cpp
@@ -54,44 +54,79 @@ Configuration::configopts Main::getConfigOpts() {
 }
 
 void Main::start(int tid) { 
-	std::stringstream temp_str,temp_str2,temp_str3;
+	std::stringstream temp_str,temp_str2,temp_str3,temp_str4;
+	std::list<const char *> params_list;
 	context.info("My pid " + QString::number(monitored_pid));
-	const char * p1= "./perf";
-	const char * p2= "--numa-migrate";
-	const char * p3= "-e";
-	const char * p4= "cpu/mem-loads/pp";
-	const char * p5= "--cpu=0-31";
-	const char * p7= "--numa-repdetail";
-	const char * p8= "3";
-	const char * p9= "--track-accesslvls";
-	const char * p10= "-c";
+	
+	//this represents the parameters sent to the SPM via command line
+	params_list.push_back("./perf");
+	params_list.push_back("--numa-migrate");
+	params_list.push_back("-e");
+	params_list.push_back("cpu/mem-loads/pp");
+	params_list.push_back("-a");
+	params_list.push_back("--numa-repdetail");
+	params_list.push_back("3");
+	params_list.push_back("--track-accesslvls");
+	params_list.push_back("-c");
 	temp_str<<(period);
 	std::string str = temp_str.str();
-	const char * p11= str.c_str();
-	const char * p12= "--weighmin";
+	params_list.push_back(str.c_str());
+	params_list.push_back("--weighmin");
 	temp_str2<<(min_weight);
 	std::string str2  = temp_str2.str();
-	const char * p13= str2.c_str();;
-	const char * p14= "--sensing-time";
+	params_list.push_back(str2.c_str());
+	params_list.push_back("--sensing-time");
 	temp_str3<<(sensing_time);
 	std::string str3 = temp_str3.str();
-	const char * p15= str3.c_str();
-	const char * p19= "--npid";
-	char p20[6];
-	sprintf(p20,"%d",monitored_pid);
-	//"--numa-migrate -e cpu/mem-loads/pp --cpu=0-31  --numa-repdetail 3 --track-accesslvls -c 500
-	// --weighmin 150 --sensing-time 20   --npid 84789";
-	const char * args[]={p1,p2,p3,p4,p5,p7,p8,p9,p10,p11,p12,p13,p14,p15,p19,p20};
+	params_list.push_back(str3.c_str());
+	params_list.push_back("--npid");
+	temp_str4<<(monitored_pid);
+	std::string str4 = temp_str4.str();
+	params_list.push_back(str4.c_str());
+	
+	int list_size=params_list.size();
+	const char** params_array=(const char**)malloc(sizeof(const char *)*list_size);
+	
+	for (int i=0; i<list_size; i++){
+		params_array[i]=params_list.front();
+		params_list.pop_front();
+	}
+	//const char * p1= "./perf";
+	//const char * p2= "--numa-migrate";
+	//const char * p3= "-e";
+	//const char * p4= "cpu/mem-loads/pp";
+	//const char * p5= "--cpu=0-31";
+	//const char * p7= "--numa-repdetail";
+	//const char * p8= "3";
+	//const char * p9= "--track-accesslvls";
+	//const char * p10= "-c";
+	//temp_str<<(period);
+	//std::string str = temp_str.str();
+	//const char * p11= str.c_str();
+	//const char * p12= "--weighmin";
+	//temp_str2<<(min_weight);
+	//std::string str2  = temp_str2.str();
+	//const char * p13= str2.c_str();;
+	//const char * p14= "--sensing-time";
+	//temp_str3<<(sensing_time);
+	//std::string str3 = temp_str3.str();
+	//const char * p15= str3.c_str();
+	//const char * p19= "--npid";
+	//char p20[6];
+	//sprintf(p20,"%d",monitored_pid);
+	////"--numa-migrate -e cpu/mem-loads/pp --cpu=0-31  --numa-repdetail 3 --track-accesslvls -c 500
+	//// --weighmin 150 --sensing-time 20   --npid 84789";
+	//const char * args[]={p1,p2,p3,p4,p5,p7,p8,p9,p10,p11,p12,p13,p14,p15,p19,p20};
 	if(!started){
-		int resp=init_numa_analysis(args, 16);
+		init_numa_analysis(params_array, list_size);
 		started=true;
 	}
-	
+	tid++; //just to get rid of warning
 	 }
 
 double Main::value(int tid) {
 	
-	return 0;
+	return tid;
 }
 
 double Main::stop(int tid) {
