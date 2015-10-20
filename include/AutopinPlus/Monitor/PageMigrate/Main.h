8d05227e22a20231d37c4fa8286cc8b38db6ad1d
Daniel Ortiz <daniel.ortiz@tum.de>
2015-10-20 14:19:27 +0200

    added missing files

---
+++ include/AutopinPlus/Monitor/PageMigrate/Main.h
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Autopin+.
+ * Copyright (C) 2015 Technische Universität München - LRR
+ *
+ * This file is licensed under the GNU General Public License Version 3
+ */
+
+#pragma once
+
+#include <AutopinPlus/PerformanceMonitor.h>
+#include <map>
+
+namespace AutopinPlus {
+namespace Monitor {
+namespace PageMigrate {
+
+/*!
+ * \brief Random performance counter
+ *
+ * This class simulates a real performance counter by generating random
+ * performance values.
+ */
+class Main : public PerformanceMonitor {
+  public:
+	/*!
+	 * \brief Constructor
+	 *
+	 * \param[in] name		Name of the monitor
+	 * \param[in] config	Pointer to the current Configuration instance
+	 * \param[in] context	Reference to the context of the object calling the constructor
+	 */
+	Main(QString name, const Configuration &config, AutopinContext &context);
+
+	void init() override;
+	void start(int tid) override;
+	double value(int tid) override;
+	double stop(int tid) override;
+	void clear(int tid) override;
+	ProcessTree::autopin_tid_list getMonitoredTasks() override;
+	Configuration::configopts getConfigOpts() override;
+
+  private:
+	/*!
+	 * Data structure for storing random performance values
+	 * for threads
+	 */
+	typedef std::map<int, double> rand_map;
+
+	/*!
+	 * The period used to make the memory loads sampling
+	 */
+	int period;
+	
+	/*!
+	 * The minimum weight for a memory access to be sampled
+	 */
+	int min_weight;
+
+	/*!
+	 * The time in seconds to look for remote accesses
+	 */
+	int sensing_time;
+	/*!
+	 * Performance values of "monitored" tasks
+	 */
+	rand_map rands;
+	
+	bool started;
+};
+
+} // namespace Random
+} // namespace Monitor
+} // namespace AutopinPlus
---
+++ src/AutopinPlus/Monitor/PageMigrate/Main.cpp
@@ -0,0 +1,98 @@
+/*
+ * This file is part of Autopin+.
+ * Copyright (C) 2015 Technische Universität München - LRR
+ *
+ * This file is licensed under the GNU General Public License Version 3
+ */
+
+#include <AutopinPlus/Monitor/PageMigrate/Main.h>
+
+#include <QTime>
+#include <stdlib.h>
+extern "C" {
+	#include "spm.h"
+}
+
+
+namespace AutopinPlus {
+namespace Monitor {
+namespace PageMigrate {
+
+Main::Main(QString name, const Configuration &config, AutopinContext &context)
+	: PerformanceMonitor(name, config, context) {
+	type = "random";
+	started=false;
+	//parameters for the memory access sampling
+	period=1000;
+	min_weight=50;
+	sensing_time=30;
+}
+
+
+	
+void Main::init() {
+	context.info("Initializing monitor \"" + name + "\" (pageMigrate)");
+
+	//Must be left, this value is read by the strategy
+	valtype = MAX;
+	// Set standard values
+
+
+	if (config.configOptionExists(name + ".sampling_period") == 1) period = config.getConfigOptionInt(name + ".sampling_period");
+	if (config.configOptionExists(name + ".min_weight") == 1) min_weight = config.getConfigOptionInt(name + ".min_weight");
+	if (config.configOptionExists(name + ".sensing_time") == 1) sensing_time = config.getConfigOptionInt(name + ".sensing_time");
+	
+	//context.info("Maximum random value " + QString::number(rand_max));
+}
+
+Configuration::configopts Main::getConfigOpts() {
+	Configuration::configopts result;
+
+	
+	result.push_back(Configuration::configopt("valtype", QStringList("MAX")));
+	return result;
+}
+
+void Main::start(int tid) { 
+	struct sampling_settings st;
+	//std::list<const char *> params_list;
+	memset(&st,0,sizeof(struct sampling_settings));
+	context.info("My pid " + QString::number(monitored_pid));
+	st.ll_sampling_period=period;
+	st.ll_weight_threshold=min_weight;
+	st.measure_time=sensing_time;
+	st.pid_uo=monitored_pid;
+	init_spm(&st);
+}
+
+double Main::value(int tid) {
+	
+	return tid;
+}
+
+double Main::stop(int tid) {
+	double result = value(tid);
+	if (context.isError()) {
+		return 0;
+	}
+
+	rands.erase(tid);
+	return result;
+}
+
+void Main::clear(int tid) {
+	if (rands.find(tid) != rands.end()) rands.erase(tid);
+}
+
+ProcessTree::autopin_tid_list Main::getMonitoredTasks() {
+	ProcessTree::autopin_tid_list result;
+
+	for (auto &elem : rands) result.insert(elem.first);
+
+	return result;
+}
+
+
+} // namespace Random
+} // namespace Monitor
+} // namespace AutopinPlus
